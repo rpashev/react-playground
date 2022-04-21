dc8490b47186d2f38ef58b2658cf10540285d589
rpashev <rossen1991@gmail.com>
2022-04-21 17:41:11 +0300

    first commit - react with scss

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import styles from "./App.module.scss";
+
+function App() {
+  return (
+    <div className={styles.app}>
+      <header className={styles.header}>Hi</header>
+    </div>
+  );
+}
+
+export default App;
